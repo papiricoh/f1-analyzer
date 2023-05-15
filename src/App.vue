cc5b8d4f566d90d40099884bc9bf131ae8409875
papiricoh <slendeman0200@gmail.com>
2023-05-15 14:16:17 +0200

    added progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import CarPage from './components/CarPage.vue'
 </script>
 
 <script lang="ts">
@@ -8,6 +8,7 @@ import { ipcRenderer } from 'electron';
 export default {
   data() {
     return {
+      page: 'car',
       motionData: null
     };
   },
@@ -24,7 +25,16 @@ export default {
 
 
 <template>
-  <div>
-
+  <div class="main">
+    <header>
+      <div class="header_button" @click="page = 'car'">CAR</div>
+      <div class="header_button" @click="page = 'standings'">STANDINGS</div>
+      <img height="60" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/F1.svg/2560px-F1.svg.png" alt="">
+      <div class="header_button" @click="page = 'strategy'">STRATEGY</div>
+      <div class="header_button" @click="page = 'config'">CONFIGURATION</div>
+    </header>
+    <div class="page">
+      <CarPage v-if="page == 'car'"></CarPage>
+    </div>
   </div>
 </template>
---
+++ src/components/CarPage.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+</script>
+
+<script lang="ts">
+
+export default {
+  data() {
+    return {
+        car: { speed: 230.3, rpm: 10532, gear: 5}
+    };
+  },
+  methods: {
+    calculateRpm() {
+        return (( this.car.rpm / 15000 ) * 100).toFixed(0);
+    }
+  }
+};
+</script>
+
+
+<template>
+  <div class="car_grid">
+    <div class="car_container">
+        <div style="text-align: center;" class="title">CAR DATA</div>
+        <div class="rpm_container">
+            <div class="rpm">
+                <div class="loading_container">
+                    <div :style="'width: ' + calculateRpm() + '%;'" class="loading_body"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="car_container">
+
+    </div>
+  </div>
+</template>
