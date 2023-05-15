13505ab80d2c14376e28cd8683e410771b39807e
papiricoh <slendeman0200@gmail.com>
2023-05-15 11:10:02 +0200

    changed name and more things

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-
-
-console.log("[App.vue]", `Hello world from Electron ${process.versions.electron}!`)
 </script>
 
 <script lang="ts">
@@ -11,16 +8,16 @@ import { ipcRenderer } from 'electron';
 export default {
   data() {
     return {
-      udpData: null
+      motionData: null
     };
   },
   mounted() {
     ipcRenderer.on('motion-data', (event, data) => {
-      this.udpData = data;
+      this.motionData = data;
     });
   },
   beforeDestroy() {
-    ipcRenderer.removeAllListeners('udp-data');
+    ipcRenderer.removeAllListeners('motion-data');
   }
 };
 </script>
@@ -28,38 +25,6 @@ export default {
 
 <template>
   <div>
-    <div>{{ udpData }}</div>
-  </div>
-  <HelloWorld msg="Electron + Vite + Vue" />
-  <div class="flex-center">
-    Place static files into the <code>/public</code> folder
-    <img style="width: 2.4em; margin-left: .4em;" src="/logo.svg" alt="Logo">
+
   </div>
 </template>
-
-<style>
-.flex-center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
