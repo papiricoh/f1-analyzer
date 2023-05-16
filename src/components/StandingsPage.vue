94c11572922a31343e10c47d25ed6f351b27b50b
Papiricoh <slendeman0200@gmail.com>
2023-05-16 23:44:28 +0200

    added distance and fastest lap css and identifier

--- src/components/StandingsPage.vue
+++ src/components/StandingsPage.vue
@@ -10,7 +10,7 @@ export default {
   data() {
     return {
         sesion_info: { lap: 1, track: "Miami", weather: 'Dry', temp: '32', type: 'race' },
-        fastest_lap: { name: 'Perez', time: '1:22.322', S1: 56.2, S2: 42.3, S3: 62.31 },
+        fastest_lap: { index: 0, time: 1000000000},
         drivers: [],
         cars: [],
         renderer_cars: [],
@@ -30,7 +30,7 @@ export default {
     lapData: function(newVal, oldVal) {
         for (let index = 0; index < newVal.length - 2; index++) {
             if(this.cars[index] != null) {
-                this.cars[index] = { pos: newVal[index].m_carPosition, name: this.drivers[index].name, team: this.drivers[index].team, tyres: this.cars[index].tyres, tyre_age: this.cars[index].tyre_age, fastest_lap: this.cars[index].fastest_lap, gap: 0, S1: newVal[index].m_sector1TimeInMS, S2: newVal[index].m_sector2TimeInMS, last_lap: newVal[index].m_lastLapTimeInMS, speed_trap: -1, current_lap: newVal[index].m_currentLapTimeInMS };
+                this.cars[index] = { pos: newVal[index].m_carPosition, name: this.drivers[index].name, team: this.drivers[index].team, tyre_age: this.cars[index].tyre_age, fastest_lap: this.cars[index].fastest_lap, gap: 0, S1: newVal[index].m_sector1TimeInMS, S2: newVal[index].m_sector2TimeInMS, last_lap: newVal[index].m_lastLapTimeInMS, speed_trap: -1, current_lap: newVal[index].m_currentLapTimeInMS, distance: newVal[index].m_lapDistance };
                 if(newVal[index].m_lastLapTimeInMS != 0 && newVal[index].m_lastLapTimeInMS < this.cars[index].fastest_lap) {
                     this.cars[index].fastest_lap = newVal[index].m_lastLapTimeInMS;
                 }
@@ -38,14 +38,16 @@ export default {
                     this.sesion_info.lap = newVal[index].m_currentLapNum;
                 }
             }else {
-                this.cars[index] = { pos: newVal[index].m_carPosition, name: this.drivers[index].name, team: this.drivers[index].team, tyres: 16, tyre_age: 0, fastest_lap: 1000000000, gap: 0, S1: newVal[index].m_sector1TimeInMS, S2: newVal[index].m_sector2TimeInMS, last_lap: newVal[index].m_lastLapTimeInMS, speed_trap: -1, current_lap: newVal[index].m_currentLapTimeInMS };
+                this.cars[index] = { pos: newVal[index].m_carPosition, name: this.drivers[index].name, team: this.drivers[index].team, tyre_age: 0, fastest_lap: 1000000000, gap: 0, S1: newVal[index].m_sector1TimeInMS, S2: newVal[index].m_sector2TimeInMS, last_lap: newVal[index].m_lastLapTimeInMS, speed_trap: -1, current_lap: newVal[index].m_currentLapTimeInMS, distance: newVal[index].m_lapDistance };
             }
         }
         this.orderList();
     },
     carStatus: function(newVal, oldVal) {
         for (let index = 0; index < this.cars.length; index++) {
-            this.cars[index].tyre = newVal.m_carStatusData[index].m_visualTyreCompound;
+            if(this.cars[index].tyre != newVal.m_carStatusData[index].m_visualTyreCompound) {
+                this.cars[index].tyre = newVal.m_carStatusData[index].m_visualTyreCompound;
+            }
             this.cars[index].tyre_age = newVal.m_carStatusData[index].m_tyresAgeLaps;
         }
     },
@@ -54,12 +56,28 @@ export default {
     orderList() {
         let new_list = Array.from(this.cars);
         let player_car = new_list[this.car_index];
+        let fastest_lap = this.fastest_lap;
+        for (let index = 0; index < new_list.length; index++) {
+            if(fastest_lap.time > new_list[index].fastest_lap) {
+                fastest_lap.time = new_list[index].fastest_lap;
+                fastest_lap.index = index;
+                fastest_lap.name = new_list[index].name;
+            }
+        }
         new_list.sort((a, b) => a.pos - b.pos);
         for (let index = 0; index < new_list.length; index++) {
             if(new_list[index] == player_car) {
                 this.render_index = index;
             }
         }
+        if(fastest_lap.name != null) {
+            for (let index = 0; index < new_list.length; index++) {
+                if(new_list[index].name == fastest_lap.name) {
+                    fastest_lap.sorted_index = index;
+                }
+            }
+        }
+        this.fastest_lap = fastest_lap;
         if(this.sesion_info.lap > 1) {
             for (let index = 1; index < new_list.length; index++) {
                 new_list[index].gap = new_list[index - 1].current_lap - new_list[index].current_lap;
@@ -112,6 +130,13 @@ export default {
             default:
                 return 'Unknown team';
         }
+    },
+    fast_lap(index) {
+        if(index == this.fastest_lap.sorted_index) {
+            return 'color: #a73ed1;';
+        }else {
+            return '';
+        }
     }
   }
 };
@@ -124,7 +149,8 @@ export default {
             <div>Type: {{ sesion_info.type }}</div>
             <div>Weather: {{ sesion_info.weather }}</div>
             <div>Temperature: {{ sesion_info.temp }}º</div>
-            <div>Fastest lap: {{ fastest_lap.name }}  -  {{ fastest_lap.time }}</div>
+            <div v-if="fastest_lap.time == 1000000000">Fastest lap: NONE</div>
+            <div v-else>Fastest lap: {{ drivers[fastest_lap.index].name }}  -  {{ new Date(fastest_lap.time).toISOString().slice(15, -1) }}</div>
         </div>
         <div class="standings_table_container">
             <div class="table_row">
@@ -145,11 +171,11 @@ export default {
                 <div>{{car.name}}</div>
                 <div>{{renderTeam(car.team)}}</div>
                 <div>
-                    <img height="30" :src="renderTyre(car.tyres)" alt="">
+                    <img height="30" :src="renderTyre(car.tyre)" alt="">
                     <div class="tyre_age">{{car.tyre_age}}</div>
                 </div>
                 <div v-if="car.fastest_lap == 1000000000">--:--</div>
-                <div v-else>{{new Date(car.fastest_lap).toISOString().slice(15, -1)}}</div>
+                <div v-else :style="fast_lap(index)">{{new Date(car.fastest_lap).toISOString().slice(15, -1)}}</div>
                 <div v-if="car.pos != 1">{{new Date(car.gap).toISOString().slice(17, -1)}}</div>
                 <div v-else> --:-- </div>
                 <div>{{new Date(car.current_lap).toISOString().slice(15, -1)}}</div>
