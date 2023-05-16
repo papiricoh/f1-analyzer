e38377e1d06adf06863921b1304e375bdf359c40
papiricoh <slendeman0200@gmail.com>
2023-05-16 11:15:27 +0200

    added f1 fonts

--- src/components/StandingsPage.vue
+++ src/components/StandingsPage.vue
@@ -5,16 +5,30 @@ export default {
   },
   data() {
     return {
-        fastest_lap: { name: 'Perez', time: '1:22:322', S1: 56.2, S2: 42.3, S3: 62.31 },
+        sesion_info: { track: "Miami", weather: 'Dry', temp: '32' },
+        fastest_lap: { name: 'Perez', time: '1:22.322', S1: 56.2, S2: 42.3, S3: 62.31 },
         cars: [
-            { pos: 1, name: 'Alonso', tyres: 'soft', tyre_age: 7, fastest_lap: '1:23:231', S1: 56.2, S2: 42.3, S3: 62.31, last_lap: '1:23:321' },
+            { pos: 1, name: 'Alonso', tyres: 'medium', tyre_age: 22, fastest_lap: '1:23.231', gap: '0:0', S1: 56.2, S2: 42.3, S3: 62.31, last_lap: '1:23.321', speed_trap: 334 },
+            { pos: 2, name: 'Perez', tyres: 'hard', tyre_age: 42, fastest_lap: '1:23.231', gap: '+0.234', S1: 56.2, S2: 42.3, S3: 62.31, last_lap: '1:23:321', speed_trap: 334 },
+            { pos: 3, name: 'Sainz', tyres: 'soft', tyre_age: 2, fastest_lap: '1:23.231', gap: '+3.522', S1: 56.2, S2: 42.3, S3: 62.31, last_lap: '1:23.321', speed_trap: 334 },
+            { pos: 4, name: 'Verstapen', tyres: 'wet', tyre_age: 22, fastest_lap: '1:23.231', gap: '+4.522', S1: 56.2, S2: 42.3, S3: 62.31, last_lap: '1:23.321', speed_trap: 334 },
         ],
     };
   },
   methods: {
     renderTyre(tyre) {
         if(tyre == 'soft') {
-            return 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/F1_tire_Pirelli_PZero_Red.svg/1024px-F1_tire_Pirelli_PZero_Red.svg.png?20190705122326';
+            return '/public/tyres/soft.svg';
+        }else if(tyre == 'medium') {
+            return '/public/tyres/medium.svg';
+        }else if(tyre == 'hard') {
+            return '/public/tyres/hard.svg';
+        }else if(tyre == 'intermediate') {
+            return '/public/tyres/intermediate.svg';
+        }else if(tyre == 'wet') {
+            return '/public/tyres/wet.svg';
+        }else {
+            return '/public/tyres/soft.svg';
         }
     }
   }
@@ -22,17 +36,25 @@ export default {
 </script>
 
 <template>
-    <div>
+    <div class="standings_page">
+        <div class="sesion_info">
+            <div>{{ sesion_info.track }} Grand Prix</div>
+            <div>Weather: {{ sesion_info.weather }}</div>
+            <div>Temperature: {{ sesion_info.temp }}º</div>
+            <div>Fastest lap: {{ fastest_lap.name }}  -  {{ fastest_lap.time }}</div>
+        </div>
         <div class="standings_table_container">
             <div class="table_row">
                 <div>POS</div>
                 <div>NAME</div>
                 <div>TYRES</div>
                 <div>FASTEST LAP</div>
+                <div>GAP</div>
                 <div>S1</div>
                 <div>S2</div>
                 <div>S3</div>
                 <div>LAST LAP</div>
+                <div>SPEED TRAP</div>
             </div>
             <div v-for="car in cars" class="table_row">
                 <div>{{car.pos}}</div>
@@ -42,10 +64,13 @@ export default {
                     <div class="tyre_age">{{car.tyre_age}}</div>
                 </div>
                 <div>{{car.fastest_lap}}</div>
+                <div v-if="car.pos != 1">{{car.gap}}</div>
+                <div v-else> --:-- </div>
                 <div>{{car.S1}}</div>
                 <div>{{car.S2}}</div>
                 <div>{{car.S3}}</div>
                 <div>{{car.last_lap}}</div>
+                <div>{{car.speed_trap}}</div>
             </div>
         </div>
     </div>
