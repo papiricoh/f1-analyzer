abf292dbf8f1a323bc64b5a7c926880b984afa0c
papiricoh <slendeman0200@gmail.com>
2023-05-23 02:57:47 +0200

    finished pit window UI

--- src/components/StrategyPage.vue
+++ src/components/StrategyPage.vue
@@ -10,6 +10,7 @@ export default {
   },
   data() {
     return {
+        current_lap: 1,
         pit_stop: { ideal_lap: 2, latest_lap: 8, rejoin_pos: 2 }
     };
   },
@@ -27,15 +28,37 @@ export default {
 <template>
   <div class="strategy_dashboard">
     <div class="grid_container pit_guide_container">
-
+        <div class="pit_window_header">PIT WINDOW</div>
+        <div v-if="current_lap >= pit_stop.ideal_lap && pit_stop.latest_lap >= current_lap" class="pit_window_status pit_window_status_blue">OPEN</div>
+        <div v-else class="pit_window_status pit_window_status_red">CLOSED</div>
+        <div style="border-top: 1px solid white; width: 70%; align-self: center;"></div>
+        <div class="estimated_laps">Laps {{pit_stop.ideal_lap}} - {{pit_stop.latest_lap}}</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.pit_guide_container {
-    grid-column: 1/2;
-    grid-row: 1/2;
+.estimated_laps {
+    color: white;
+    text-align: center;
+    font-size: 1.5rem;
+}
+.pit_window_status_blue {
+    color: rgb(43, 196, 223);
+    text-align: center;
+}
+.pit_window_status_red {
+    color: red;
+    text-align: center;
+}
+.pit_window_status {
+    margin-top: 1.4rem;
+    font-size: 3rem;
+}
+.pit_window_header {
+    text-align: center;
+    color: white;
+    font-size: 1.4rem;
 }
 .grid_container {
     background-color: #03071e;
@@ -45,6 +68,13 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: stretch;
+    gap: .6rem;
+}
+.pit_guide_container {
+    grid-column: 1/2;
+    grid-row: 1/2;
+    justify-content: stretch;
+    gap: 1.6rem;
 }
 .strategy_dashboard {
     display: grid;
