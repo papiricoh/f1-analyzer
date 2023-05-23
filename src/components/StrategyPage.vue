606fc9e9f2c72ddfc6003a890dd10672c25f8ad7
papiricoh <slendeman0200@gmail.com>
2023-05-23 02:29:09 +0200

    added StragegyPage

---
+++ src/components/StrategyPage.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<script>
+
+export default {
+  props: {
+    
+  },
+  data() {
+    return {
+        
+    };
+  },
+  watch: {
+    
+  },
+  methods: {
+    
+  },
+  computed: {
+
+  }
+};
+</script>
+<template>
+  <div class="strategy_dashboard">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
