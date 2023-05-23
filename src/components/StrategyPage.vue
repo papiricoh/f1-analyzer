09a090c648f4f0d19187209b7d5310eb7a79b407
papiricoh <slendeman0200@gmail.com>
2023-05-23 02:38:59 +0200

    added first container to strategy

--- src/components/StrategyPage.vue
+++ src/components/StrategyPage.vue
@@ -10,7 +10,7 @@ export default {
   },
   data() {
     return {
-        
+        pit_stop: { ideal_lap: 2, latest_lap: 8, rejoin_pos: 2 }
     };
   },
   watch: {
@@ -26,14 +26,31 @@ export default {
 </script>
 <template>
   <div class="strategy_dashboard">
+    <div class="grid_container pit_guide_container">
 
+    </div>
   </div>
 </template>
 
 <style scoped>
+.pit_guide_container {
+    grid-column: 1/2;
+    grid-row: 1/2;
+}
+.grid_container {
+    background-color: #03071e;
+    border-radius: .3rem;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.335);
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+}
 .strategy_dashboard {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-template-rows: 1fr 1fr 1fr;
+    height: 60rem;
+    gap: 1rem;
 }
 </style>
